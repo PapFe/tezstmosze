99a2ce5c6ba70dcc763e57cad184aa75c42129d7
PapFe <43848351+PapFe@users.noreply.github.com>
2020-09-27 19:33:04 +0200

    na

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
 
-if test -f out_res.txt then
+if test -f out_res.txt
+then
 	rm out_res.txt
 fi
 
-for i in units/*.json do
- for j in units/*.json  do
-		if [ $i != $j ] then
+for i in units/*.json 
+do
+	for j in units/*.json
+	do
+		if [ $i != $j ]
+		then
 			main $i $j >> out_res.txt
 		fi
- done
+	done
 done
