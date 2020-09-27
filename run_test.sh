2dfbe98f1519cb8ef4cddc680d68e078563cab8d
PapFe <43848351+PapFe@users.noreply.github.com>
2020-09-27 19:26:54 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -4,8 +4,8 @@ if test -f out_res.txt then
 	rm out_res.txt
 fi
 
-for i in units/*.json do
- for j in units/*.json  do
+for i in //units//*.json do
+ for j in //units//*.json  do
 		if [ $i != $j ] then
 			main $i $j >> out_res.txt
 		fi
